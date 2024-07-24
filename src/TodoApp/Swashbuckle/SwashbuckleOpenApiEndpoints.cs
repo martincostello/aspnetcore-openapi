cd4c70d657de26352390ec5b4878c07089f74773
martincostello <martin@martincostello.com>
2024-07-24 12:43:08 +0100

    Add initial Swashbuckle implementation
    Add basic implementation for Swashbuckle.AspNetCore.

--- src/TodoApp/Swashbuckle/SwashbuckleOpenApiEndpoints.cs
+++ src/TodoApp/Swashbuckle/SwashbuckleOpenApiEndpoints.cs
@@ -1,20 +1,65 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using Microsoft.OpenApi.Models;
+
 namespace TodoApp;
 
 public static class SwashbuckleOpenApiEndpoints
 {
     public static IServiceCollection AddSwashbuckleOpenApi(this IServiceCollection services)
     {
-        // TODO
+        services.AddSwaggerGen((options) =>
+        {
+            var info = new OpenApiInfo()
+            {
+                Contact = new()
+                {
+                    Name = "Martin Costello",
+                    Url = new("https://www.martincostello.com"),
+                },
+                License = new()
+                {
+                    Name = "Apache 2.0",
+                    Url = new("https://www.apache.org/licenses/LICENSE-2.0"),
+                },
+                Title = "Todo API (Swashbuckle.AspNetCore)",
+                Version = "v1"
+            };
+
+            options.EnableAnnotations();
+            options.IgnoreObsoleteActions();
+            options.IgnoreObsoleteProperties();
+
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "TodoApp.xml"));
+
+            options.SwaggerDoc("v1", info);
+
+            var scheme = new OpenApiSecurityScheme()
+            {
+                BearerFormat = "JSON Web Token",
+                Description = "Bearer authentication using a JWT.",
+                Scheme = "bearer",
+                Type = SecuritySchemeType.Http,
+                Reference = new()
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme,
+                },
+                UnresolvedReference = false,
+            };
+            options.AddSecurityDefinition(scheme.Reference.Id, scheme);
+            options.AddSecurityRequirement(new() { [scheme] = Array.Empty<string>() });
+        });
+
         return services;
     }
 
     public static T UseSwashbuckleOpenApi<T>(this T builder)
         where T : IApplicationBuilder, IEndpointRouteBuilder
     {
-        // TODO
+        builder.UseSwagger();
+
         return builder;
     }
 }
