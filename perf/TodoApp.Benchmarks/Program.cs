0b57abe8bf91b0173852893e1adc77849955fd66
martincostello <martin@martincostello.com>
2024-08-07 11:00:27 +0100

    Debug benchmarks
    Add a `--test` flag to support running the benchmarks under the debugger to test their behaviour.

--- perf/TodoApp.Benchmarks/Program.cs
+++ perf/TodoApp.Benchmarks/Program.cs
@@ -1,7 +1,26 @@
-// Copyright (c) Martin Costello, 2024. All rights reserved.
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using BenchmarkDotNet.Running;
 using TodoApp;
 
-BenchmarkRunner.Run<OpenApiBenchmarks>(args: args);
+if (args.SequenceEqual(["--test"]))
+{
+    await using var benchmarks = new OpenApiBenchmarks();
+    await benchmarks.StartServer();
+
+    try
+    {
+        _ = await benchmarks.AspNetCore();
+        _ = await benchmarks.NSwag();
+        _ = await benchmarks.Swashbuckle();
+    }
+    finally
+    {
+        await benchmarks.StopServer();
+    }
+}
+else
+{
+    BenchmarkRunner.Run<OpenApiBenchmarks>(args: args);
+}
