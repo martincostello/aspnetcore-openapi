417f5c6291471f4e79decab02d744f82b8ddda2d
martincostello <martin@martincostello.com>
2024-08-07 10:48:25 +0100

    Add benchmarks
    Add benchmarks for rendering an OpenAPI document using ASP.NET Core OpenAPI, NSwag and Swashbuckle.AspNetCore.

--- src/TodoApp/Program.cs
+++ src/TodoApp/Program.cs
@@ -1,57 +1,19 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using Microsoft.AspNetCore.HttpOverrides;
 using TodoApp;
 
 // Create the default web application builder
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure the Todo repository and associated services
-builder.Services.AddTodoApi();
-
-// Add Razor Pages to render the UI
-builder.Services.AddRazorPages();
-
-// Configure OpenAPI documentation for the Todo API
-builder.Services.AddOpenApiServices();
-
-if (string.Equals(builder.Configuration["CODESPACES"], "true", StringComparison.OrdinalIgnoreCase))
-{
-    // When running in GitHub Codespaces, X-Forwarded-Host also needs to be set
-    builder.Services.Configure<ForwardedHeadersOptions>(
-        options => options.ForwardedHeaders |= ForwardedHeaders.XForwardedHost);
-}
+// Add TodoApp into the web application builder
+builder.AddTodoApp();
 
 // Create the app
 var app = builder.Build();
 
-// Configure error handling
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/error");
-}
-
-app.UseStatusCodePagesWithReExecute("/error", "?id={0}");
-
-// Require use of HTTPS in production
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-    app.UseHttpsRedirection();
-}
-
-// Add static files for JavaScript, CSS and OpenAPI
-app.UseStaticFiles();
-
-// Add endpoints for OpenAPI
-app.UseOpenApiEndpoints();
-
-// Add the HTTP endpoints
-app.MapTodoApiRoutes();
-
-// Add Razor Pages for the UI
-app.MapRazorPages();
+// Use TodoApp middleware and endpoints with the web application
+app.UseTodoApp();
 
 // Run the application
 app.Run();
---
+++ src/TodoApp/TodoAppBuilder.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace TodoApp;
+
+public static class TodoAppBuilder
+{
+    public static WebApplicationBuilder AddTodoApp(this WebApplicationBuilder builder)
+    {
+        // Configure the Todo repository and associated services
+        builder.Services.AddTodoApi();
+
+        // Add Razor Pages to render the UI
+        builder.Services.AddRazorPages();
+
+        // Configure OpenAPI documentation for the Todo API
+        builder.Services.AddOpenApiServices();
+
+        if (string.Equals(builder.Configuration["CODESPACES"], "true", StringComparison.OrdinalIgnoreCase))
+        {
+            // When running in GitHub Codespaces, X-Forwarded-Host also needs to be set
+            builder.Services.Configure<ForwardedHeadersOptions>(
+                options => options.ForwardedHeaders |= ForwardedHeaders.XForwardedHost);
+        }
+
+        return builder;
+    }
+
+    public static WebApplication UseTodoApp(this WebApplication app)
+    {
+        // Configure error handling
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/error");
+        }
+
+        app.UseStatusCodePagesWithReExecute("/error", "?id={0}");
+
+        // Require use of HTTPS in production
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseHsts();
+            app.UseHttpsRedirection();
+        }
+
+        // Add static files for JavaScript, CSS and OpenAPI
+        app.UseStaticFiles();
+
+        // Add endpoints for OpenAPI
+        app.UseOpenApiEndpoints();
+
+        // Add the HTTP endpoints
+        app.MapTodoApiRoutes();
+
+        // Add Razor Pages for the UI
+        app.MapRazorPages();
+
+        return app;
+    }
+}
