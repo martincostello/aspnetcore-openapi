1c4d25fcc1a26cca3647bc082fb0d858e7b3a6d2
martincostello <martin@martincostello.com>
2025-03-05 13:15:18 +0000

    Fix build
    Update to a build of Swashbuckle.AspNetCore that supports ASP.NET Core 10 and Microsoft.OpenApi v2.

--- src/TodoApp/OpenApi/ExampleFormatter.cs
+++ src/TodoApp/OpenApi/ExampleFormatter.cs
@@ -2,8 +2,8 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
-using Microsoft.OpenApi.Any;
 
 namespace TodoApp.OpenApi;
 
@@ -19,9 +19,9 @@ internal static class ExampleFormatter
     /// <typeparam name="TProvider">The type of the example provider.</typeparam>
     /// <param name="context">The JSON serializer context to use.</param>
     /// <returns>
-    /// The <see cref="IOpenApiAny"/> to use as the example.
+    /// The <see cref="JsonNode"/> to use as the example.
     /// </returns>
-    public static IOpenApiAny AsJson<TSchema, TProvider>(JsonSerializerContext context)
+    public static JsonNode? AsJson<TSchema, TProvider>(JsonSerializerContext context)
         where TProvider : IExampleProvider<TSchema>
         => AsJson(TProvider.GenerateExample(), context);
 
@@ -32,87 +32,12 @@ internal static class ExampleFormatter
     /// <param name="example">The example value to format as JSON.</param>
     /// <param name="context">The JSON serializer context to use.</param>
     /// <returns>
-    /// The <see cref="IOpenApiAny"/> to use as the example.
+    /// The <see cref="JsonNode"/> to use as the example.
     /// </returns>
-    public static IOpenApiAny AsJson<T>(T example, JsonSerializerContext context)
+    public static JsonNode? AsJson<T>(T example, JsonSerializerContext context)
     {
         // Apply any formatting rules configured for the API (e.g. camel casing)
         var json = JsonSerializer.Serialize(example, typeof(T), context);
-        using var document = JsonDocument.Parse(json);
-
-        if (document.RootElement.ValueKind == JsonValueKind.String)
-        {
-            return new OpenApiString(document.RootElement.ToString());
-        }
-
-        var result = new OpenApiObject();
-
-        // Recursively build up the example from the properties of the object
-        foreach (var token in document.RootElement.EnumerateObject())
-        {
-            if (TryParse(token.Value, out var any))
-            {
-                result[token.Name] = any;
-            }
-        }
-
-        return result;
-    }
-
-    private static bool TryParse(JsonElement token, out IOpenApiAny? any)
-    {
-        any = null;
-
-        switch (token.ValueKind)
-        {
-            case JsonValueKind.Array:
-                var array = new OpenApiArray();
-
-                foreach (var value in token.EnumerateArray())
-                {
-                    if (TryParse(value, out var child))
-                    {
-                        array.Add(child);
-                    }
-                }
-
-                any = array;
-                return true;
-
-            case JsonValueKind.False:
-                any = new OpenApiBoolean(false);
-                return true;
-
-            case JsonValueKind.True:
-                any = new OpenApiBoolean(true);
-                return true;
-
-            case JsonValueKind.Number:
-                any = new OpenApiDouble(token.GetDouble());
-                return true;
-
-            case JsonValueKind.String:
-                any = new OpenApiString(token.GetString());
-                return true;
-
-            case JsonValueKind.Object:
-                var obj = new OpenApiObject();
-
-                foreach (var child in token.EnumerateObject())
-                {
-                    if (TryParse(child.Value, out var value))
-                    {
-                        obj[child.Name] = value;
-                    }
-                }
-
-                any = obj;
-                return true;
-
-            case JsonValueKind.Null:
-            case JsonValueKind.Undefined:
-            default:
-                return false;
-        }
+        return JsonNode.Parse(json);
     }
 }
