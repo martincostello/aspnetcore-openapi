4e5a15f05aef1b4300cf4775c2c7be6cd8b51af4
martincostello <martin@martincostello.com>
2025-05-23 10:43:51 +0100

    Update to .NET 10 preview 4
    Update to preview 4 of .NET 10.

--- src/TodoApp/OpenApi/OpenApiExampleAttribute`2.cs
+++ src/TodoApp/OpenApi/OpenApiExampleAttribute`2.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
-using Microsoft.OpenApi.Any;
 
 namespace TodoApp.OpenApi;
 
@@ -30,6 +30,6 @@ public class OpenApiExampleAttribute<TSchema, TProvider> : Attribute, IOpenApiEx
     object? IOpenApiExampleMetadata.GenerateExample() => GenerateExample();
 
     /// <inheritdoc/>
-    IOpenApiAny IOpenApiExampleMetadata.GenerateExample(JsonSerializerContext context)
+    JsonNode? IOpenApiExampleMetadata.GenerateExample(JsonSerializerContext context)
         => ExampleFormatter.AsJson(GenerateExample(), context);
 }
