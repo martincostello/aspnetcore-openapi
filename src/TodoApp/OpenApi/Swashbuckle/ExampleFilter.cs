3b870c583e119a17b006ea95ab1c2337fbad1990
martincostello <martin@martincostello.com>
2025-07-15 16:59:46 +0100

    Update to ASP.NET Core 10 preview 6
    Update to preview 6 of ASP.NET Core 10.

--- src/TodoApp/OpenApi/Swashbuckle/ExampleFilter.cs
+++ src/TodoApp/OpenApi/Swashbuckle/ExampleFilter.cs
@@ -1,8 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using Microsoft.OpenApi.Models;
-using Microsoft.OpenApi.Models.Interfaces;
+using Microsoft.OpenApi;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace TodoApp.OpenApi.Swashbuckle;
